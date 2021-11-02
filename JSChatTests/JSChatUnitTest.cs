199f9e83caab6dfa87b009d0c0a40887f1eae559
ahernadez <ahernandez@servsuite.onmicrosoft.com>
2021-11-01 20:56:00 -0500

    UnitTest added

---
+++ JSChatTests/JSChatUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using JSChat.Controllers;
+using JSChatModel.ViewModels;
+using System.Web.Mvc;
+
+namespace JSChatTests
+{
+    [TestClass]
+    public class JSChatUnitTest
+    {
+        [TestMethod]
+        public void CreateMethodTest()
+        {
+            HomeController homeController = new HomeController();
+            ActionResult actionResult = homeController.Create(new CreateMessageViewModel
+            {
+                Message = "Test Message",
+                UserId = new Guid().ToString(),
+                UserName = "TestUser"
+            });
+            Assert.IsNotNull(actionResult);
+        }
+
+        [TestMethod]
+        public void GetStockInfoTest()
+        {
+            string stock_code = "aapl.us";
+
+            JSChatServices.BotServices BotServicesTest = new JSChatServices.BotServices();
+
+            StockModel stockModel = BotServicesTest.GetStockInfo(stock_code);
+
+            Assert.IsNotNull(stockModel); 
+
+        }
+    }
+}
---
+++ JSChatTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JSChatTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JSChatTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0d718c8-d4df-4fa8-990c-3fb5e9ad77cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
